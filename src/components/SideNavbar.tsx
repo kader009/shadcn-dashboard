b6aa68fee0454bdd8dba465f5a0d0e271862562a
kader009 <kadermollarasel000@gmail.com>
2024-04-21 18:06:05 +0600

    create table page

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -14,6 +14,7 @@ import {
   ShoppingBasket,
   CalendarDays,
   LogIn,
+  Table2
 } from 'lucide-react';
 import { Button } from './ui/button';
 
@@ -77,9 +78,9 @@ export default function SideNavbar({}: Props) {
             variant: 'ghost',
           },
           {
-            title: 'Map',
-            href: '/map',
-            icon: CalendarDays,
+            title: 'Table',
+            href: '/table',
+            icon: Table2,
             variant: 'ghost',
           },
           {
